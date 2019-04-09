fff4b35d652888164d4aba68113370eeacb1d97b
luckygamerv2 <45069336+luckygamerv2@users.noreply.github.com>
2019-04-09 08:18:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+ 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(' made by  luckygamer#0111`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+ client.user.setGame("made by luckygamer#0111", "https://www.twitch.tv/Alpha");
 });
-
-
-
-
-
-
-
-
-
+ 
+ 
+ 
+ 
 
 
 
@@ -87,5 +61,4 @@ client.on('message', message => {
     })
     }
     });
-
-client.login(process.env.BOT_TOKEN);
+client.login('NTY0ODQ4NzM5MzA3NDg3MjQy.XKwocg.VXqyV9Yf-o3HT0hOL0pqLFnAp-I');
